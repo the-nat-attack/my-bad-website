554007f78ec991bd1a653ef8ca192cb0428546d8
Natalie <natalie.lacroix@uconn.edu>
2024-03-06 14:35:12 -0500

    demo day commit

---
+++ js/main.js
@@ -0,0 +1,113 @@
+/*TIMER*/
+var minute = 0
+var second = 0
+var count = 0
+var timer = true
+
+if (localStorage.getItem("minString") !== null) {
+    minute = parseInt(localStorage.getItem("minString"))
+}
+if (localStorage.getItem("secString") !== null) {
+    second = parseInt(localStorage.getItem("secString"))
+}
+if (localStorage.getItem("countString") !== null) {
+    count = parseInt(localStorage.getItem("countString"))
+}
+
+function stopBtn() { 
+    timer = false; 
+};
+
+function resetWatch() { 
+    timer = false; 
+    minute = 0; 
+    second = 0; 
+    count = 0; 
+    localStorage.setItem('heart', 3)
+    localStorage.removeItem("minString")
+    localStorage.removeItem("secString")
+    localStorage.removeItem("countString")
+    document.getElementById('min').innerHTML = "00"; 
+    document.getElementById('sec').innerHTML = "00"; 
+    document.getElementById('count').innerHTML = "00"; 
+    timer = true; 
+    stopWatch();
+}; 
+  
+function stopWatch() { 
+    if (timer) { 
+        count++; 
+  
+        if (count == 100) { 
+            second++; 
+            count = 0; 
+        } 
+  
+        if (second == 60) { 
+            minute++; 
+            second = 0; 
+        } 
+  
+        if (minute == 60) { 
+            hour++; 
+            minute = 0; 
+            second = 0; 
+        } 
+  
+        localStorage.setItem("minString",minute)
+        localStorage.setItem("secString",second)
+        localStorage.setItem("countString",count)
+  
+        if (minute < 10) { 
+            localStorage.setItem("minString", "0" + localStorage.getItem("minString"))
+        } 
+  
+        if (second < 10) { 
+            localStorage.setItem("secString", "0" + localStorage.getItem("secString")); 
+        } 
+  
+        if (count < 10) { 
+            localStorage.setItem("countString", "0" + localStorage.getItem("countString")); 
+        } 
+  
+        document.getElementById('min').innerHTML = localStorage.getItem("minString"); 
+        document.getElementById('sec').innerHTML = localStorage.getItem("secString"); 
+        document.getElementById('count').innerHTML = localStorage.getItem("countString"); 
+        setTimeout(stopWatch, 10); 
+    } 
+}
+
+function noescape(){
+    alert("There is no escape.")
+}
+
+function health(){
+    for (let i = 0; i < localStorage.getItem("heart"); i++){
+        const image = document.createElement('img');
+        image.setAttribute('src', '../img/heart.webp');
+        image.setAttribute('width', '50px');
+        image.setAttribute('alt','heart');
+        image.setAttribute('id','heart-img');
+        document.getElementById('health').appendChild(image);
+    }
+}
+
+function delHealth(){
+    localStorage.setItem('heart',localStorage.getItem('heart')-1)
+    document.getElementById('heart-img').remove()
+    if (localStorage.getItem('heart') <= 0){
+        window.location.replace("../game-over.html") 
+    }
+}
+
+function pageLoad(){
+    stopWatch()
+    health()
+}
+
+function endpageLoad(){
+    stopWatch()
+    stopBtn()
+}
+
+
